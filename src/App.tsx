e797e80f6bfb5d517353780a595ad0af063d1699
Chau <chauj@vimny-chauJ-lm.local>
2018-08-03 16:12:53 -0400

    add connection to reddit api

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,21 @@
 import * as React from 'react';
-import { Provider } from 'react-redux';
-import { Link, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
-import About from './About';
-import Home from './Home';
-import Users from './Users';
+/* import About from './About'; */
+// import Users from './Users';
+import Login from './Login';
 
-import users from './containers/users';
-import store from './store';
+// import users from './containers/users';
+// import store from './store';
 
-const ConnectedUsers = users(Users);
+// const ConnectedUsers = users(Users);
 
 const App: React.SFC = () => (
-  <Provider store={store}>
-    <React.Fragment>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/users">Users</Link>
-      </nav>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={About} />
-        <Route path="/users" component={ConnectedUsers} />
-      </Switch>
-    </React.Fragment>
-  </Provider>
+  < React.Fragment >
+    <Switch>
+      <Route exact path="/" component={Login} />
+    </Switch>
+  </React.Fragment >
 );
 
 export default App;
---
+++ src/Login.tsx
@@ -0,0 +1,64 @@
+import * as React from 'react';
+import { RouteComponentProps } from 'react-router-dom';
+import { parse } from 'query-string';
+
+const clientID = 'r-olaTpMhjY8AQ'
+const redirectURI = 'http://localhost:3000'
+
+const randomString = (length: number) => {
+    let text = ''
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    for (let i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length))
+    }
+    return text
+}
+
+const authorizationURL =
+    `https://www.reddit.com/api/v1/authorize` +
+    `?client_id=${clientID}` +
+    `&response_type=code` +
+    `&state=${randomString(10)}` +
+    `&redirect_uri=${redirectURI}` +
+    `&duration=permanent` +
+    `&scope=submit`
+
+interface IProps extends RouteComponentProps<{}> {
+    isAuthenticated: boolean
+}
+
+class Login extends React.Component<IProps, {}> {
+    componentWillMount() {
+        // redirect to '/post' if authentiated
+        if (this.props.isAuthenticated) {
+            this.props.history.push('/post')
+        }
+    }
+
+    componentDidMount() {
+        // check auth state and fetch OAuth token when
+        // redirected back to this page from reddit.com
+        const { state, code, error } = parse(this.props.location.search)
+        if (state && code) {
+            if (state === localStorage.getItem('authState')) {
+                this.boundActionCreators.initializeToken(code)
+            } else {
+                console.log('Authorization Error: Invalid state.')
+            }
+            if (state && error) console.log(error)
+        }
+    }
+
+    public render() {
+        return (
+            <div>
+                <h1>Login</h1>
+                <p>Connect to your Reddit account.</p>
+                <a href={authorizationURL}>Connect</a>
+            </div >
+        )
+    }
+
+}
+
+export default Login;
