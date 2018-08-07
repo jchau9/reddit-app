582ef6ffd449279e48a391791c2bddfd2e49ffbc
Jenny Chau <jchau9@uci.edu>
2018-08-07 02:08:31 -0400

    did something and nothing

--- src/Login.tsx
+++ src/Login.tsx
@@ -18,7 +18,7 @@ const authorizationURL =
     `https://www.reddit.com/api/v1/authorize` +
     `?client_id=${clientID}` +
     `&response_type=code` +
-    `&state=${sessionStorage.getItem('authState')}` +
+    `&state=${localStorage.getItem('authState')}` +
     `&redirect_uri=${redirectURI}` +
     `&duration=permanent` +
     `&scope=submit`
@@ -28,44 +28,37 @@ class Login extends React.Component<RouteComponentProps<{}>, {}> {
         loading: false,
         token: null,
         error: null,
-        isAuthenticated: false
+        isAuthenticated: false,
     }
-
-    getToken = async (code: any) => {
-        let url = `https://www.reddit.com/api/v1/access_token`
-        /* url += `?grant_type=authorization_code&code=${code}`
-        url += `&redirect_uri=${redirectURI}` */
-        this.setState(state => ({...state, loading: true}));
-        try{
-            const res = await fetch(url, {
-                method: 'POST',
-                body: `grant_type=authorization_code&code=${code}&redirect_uri=${redirectURI}`
-            });
-            const json = await res.json();
-            this.setState(state => ({...state, loading: false, token: json.message}));
-        } catch (e) {
-            this.setState(state => ({...state, loading: false, err:e}));
-        }
-    };
 
     componentWillMount() {
         // redirect to '/post' if authentiated
         if (this.state.isAuthenticated) {
             this.props.history.push('/post');
             // if not authenticated, make sure that authState is set
-        } else if (!sessionStorage.getItem('authState')) {
-            sessionStorage.setItem('authState', randomString(10))
+        } else if (!localStorage.getItem('authState')) {
+            localStorage.setItem('authState', randomString(10))
         }
     }
     
-    componentDidMount() {
+    async componentDidMount() {
         // check auth state and fetch OAuth token when
         // redirected back to this page from reddit.com
         const { state, code, error } = parse(this.props.location.search)
         if (state && code) {
-            if (state === sessionStorage.getItem('authState')) {
-                //this.getToken(code);
-                this.state.isAuthenticated = true;
+            if (state === localStorage.getItem('authState')) {
+                let url = `https://www.reddit.com/api/v1/access_token`
+                this.setState(state => ({...state, loading: true}));
+                try{
+                    const res = await fetch(url, {
+                        method: 'POST',
+                        body: `grant_type=authorization_code&code=${code}&redirect_uri=${redirectURI}`,
+                    });
+                    const json = await res.json();
+                    this.setState(state => ({...state, loading: false, token: json.message, isAuthenticated: true}));
+                } catch (e) {
+                    this.setState(state => ({...state, loading: false, err:e}));
+                }
                 console.log(code);
                 console.log(this.state.token);
                 this.props.history.push('/post');
@@ -87,8 +80,4 @@ class Login extends React.Component<RouteComponentProps<{}>, {}> {
     }
 }
 
-/* const mapStateToProps = (state: IState) => ({
-    isAuthenticated: state.auth.isAuthenticated
-})
- */
 export default Login;
